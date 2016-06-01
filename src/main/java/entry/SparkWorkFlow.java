3c40cdce9f4727e9db46a93f9b3846e0f98c29e3
Karelgit <huanghai@simpletech.com.cn>
2016-06-01 18:07:12 +0800

    AnalysisModel Spark conf&Hbase scan未完成_2016-6-1 18:07:07

--- src/main/java/entry/SparkWorkFlow.java
+++ src/main/java/entry/SparkWorkFlow.java
@@ -1,46 +1,76 @@
 package entry;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Scan;
-import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
-import org.apache.hadoop.hbase.protobuf.ProtobufUtil;
-import org.apache.hadoop.hbase.protobuf.generated.ClientProtos;
-import org.apache.hadoop.hbase.util.Base64;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import util.HbasePoolUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 
 /**
  * Created by Administrator on 2016/5/30.
  */
 public class SparkWorkFlow {
+    private static Configuration conf = null;
+
     public static void check() {
+        //SparkConf设置
+        SparkConf sparkConf = new SparkConf();
+        sparkConf.setMaster("local");
+        sparkConf.setAppName("Spark HBase");
+        JavaSparkContext context = new JavaSparkContext(sparkConf);
 
-        JavaSparkContext sc = new JavaSparkContext("local", "hbaseTest",
-                System.getenv("SPARK_HOME"), System.getenv("JARS"));
-        Configuration conf = HBaseConfiguration.create();
+
+
+
+//        JavaSparkContext jsc = new JavaSparkContext();
+
+//        JavaRDD<String> text =
+
+        //Scan操作
         Scan scan = new Scan();
-        scan.addFamily(Bytes.toBytes("cf"));
-        scan.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("airName"));
-        try {
-            String tableName = "flight_wap_order_log";
-            conf.set(TableInputFormat.INPUT_TABLE, tableName);
-            ClientProtos.Scan proto = ProtobufUtil.toScan(scan);
-            String ScanToString = Base64.encodeBytes(proto.toByteArray());
-            conf.set(TableInputFormat.SCAN, ScanToString);
-            JavaPairRDD<ImmutableBytesWritable, Result> myRDD =
-                    sc.newAPIHadoopRDD(conf, TableInputFormat.class,
-                            ImmutableBytesWritable.class, Result.class);
+        scan.setStartRow(Bytes.toBytes("0120140722"));
+        scan.setStopRow(Bytes.toBytes("1620140728"));
+        scan.addFamily(Bytes.toBytes("basic"));
+        scan.addColumn(Bytes.toBytes("basic"), Bytes.toBytes("name"));
 
-            System.out.println(myRDD.count());
-        } catch (Exception e) {
-                e.printStackTrace();
+        conf = HbasePoolUtils.getConfiguration();
+        conf.set(TableInputFormat.INPUT_TABLE, "gycrawler");
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(out);
+        /*scan.write(dos);
+        String scanStr = Base64.encodeBytes(out.toByteArray());
+        IOUtils.closeQuietly(dos);
+        IOUtils.closeQuietly(out);
+        //高版本可以用如下方式：
+        //ClientProtos.Scan proto = ProtobufUtil.toScan(scan);
+        //String scanStr = Base64.encodeBytes(proto.toByteArray());
+        conf.set(TableInputFormat.SCAN, scanStr);
+
+        JavaPairRDD<ImmutableBytesWritable, Result> hBaseRDD = context
+                .newAPIHadoopRDD(conf, TableInputFormat.class,
+                        ImmutableBytesWritable.class, Result.class);
+
+        Long count = hBaseRDD.count();
+        System.out.println("count: " + count);
+
+        List<Tuple2<ImmutableBytesWritable, Result>> tuples = hBaseRDD
+                .take(count.intValue());
+        for (int i = 0, len = count.intValue(); i < len; i++) {
+            Result result = tuples.get(i)._2();
+            KeyValue[] kvs = result.raw();
+            for (KeyValue kv : kvs) {
+                System.out.println("rowkey:" + new String(kv.getRow()) + " cf:"
+                        + new String(kv.getFamily()) + " column:"
+                        + new String(kv.getQualifier()) + " value:"
+                        + new String(kv.getValue()));
             }
-
-
+        }*/
     }
 
     public static void main(String[] args) {
