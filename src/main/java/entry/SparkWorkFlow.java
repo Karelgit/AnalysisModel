b6f671446bf2a3e538175ff0018040e2c69b78e5
Karel <karel0522@gmail.com>
2016-06-05 22:04:36 +0800

    空白栏目检测,输入url/html,输出是空白栏目的URL_2016-06-05 22:04:28

--- src/main/java/entry/SparkWorkFlow.java
+++ src/main/java/entry/SparkWorkFlow.java
@@ -1,23 +1,23 @@
 package entry;
 
-import com.twitter.chill.Base64;
+import empty.EmptyCheck;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.KeyValue;
-import org.apache.hadoop.hbase.client.HTableInterface;
-import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.client.ResultScanner;
-import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
 import org.apache.hadoop.hbase.protobuf.ProtobufUtil;
 import org.apache.hadoop.hbase.protobuf.generated.ClientProtos;
+import org.apache.hadoop.hbase.util.Base64;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.security.SaslOutputStream;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.api.java.function.VoidFunction;
 import org.jruby.javasupport.Java;
@@ -26,6 +26,8 @@ import scala.Tuple2;
 import util.HbasePoolUtils;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2016/5/30.
@@ -42,6 +44,8 @@ public class SparkWorkFlow {
         Scan scan = new Scan();
         scan.addFamily(Bytes.toBytes("crawlerData"));
         scan.addColumn(Bytes.toBytes("crawlerData"),Bytes.toBytes("url"));
+        scan.addColumn(Bytes.toBytes("crawlerData"),Bytes.toBytes("html"));
+        scan.setMaxVersions(1);
 
         conf.set(TableInputFormat.INPUT_TABLE,"gycrawler");
         ClientProtos.Scan proto = null;
@@ -57,53 +61,40 @@ public class SparkWorkFlow {
         JavaPairRDD<ImmutableBytesWritable, Result> myRDD =
                 jsc.newAPIHadoopRDD(conf, TableInputFormat.class, ImmutableBytesWritable.class, Result.class);
 
-        System.out.println("数目:" + myRDD.count());
-
         //从Result中取出text数据
-        JavaPairRDD<String, String> text   = myRDD.mapToPair(
-                new PairFunction<Tuple2<ImmutableBytesWritable,Result>,String,String>(){
-                    @Override
+        JavaPairRDD<String, String> data   = myRDD.mapToPair(
+                new PairFunction<Tuple2<ImmutableBytesWritable,Result>,String, String>(){
                     public Tuple2<String, String> call(Tuple2<ImmutableBytesWritable, Result> immutableBytesWritableResultTuple2) throws Exception {
-                        byte [] text = immutableBytesWritableResultTuple2._2().getValue(Bytes.toBytes("crawlerData"),Bytes.toBytes("text"));
                         byte [] url  = immutableBytesWritableResultTuple2._2().getValue(Bytes.toBytes("crawlerData"),Bytes.toBytes("url"));
-                        if (text != null)   {
-                            return new Tuple2<String,String>(Bytes.toString(url), Bytes.toString(text));
-                        }else   {
-                            return null;
+                        byte [] html = immutableBytesWritableResultTuple2._2().getValue(Bytes.toBytes("crawlerData"),Bytes.toBytes("html"));
+                        return new Tuple2<String,String>(Bytes.toString(url),Bytes.toString(html));
+                    }
+                }
+        ).cache();
+
+        //过滤出空白栏目
+        JavaPairRDD<String, String> emptyTopic = data.filter(
+                new Function<Tuple2<String, String>,Boolean>()  {
+                    public Boolean call(Tuple2<String, String> stringStringTuple2) throws Exception {
+                        if(new EmptyCheck(stringStringTuple2._2(),stringStringTuple2._1()).isEmpty()) {
+                            return true;
+                        }else {
+                            return false;
                         }
                     }
                 }
         );
 
+        JavaRDD<String> emptyUrl = emptyTopic.map(
+                new Function<Tuple2<String, String>, String>() {
+                    public String call(Tuple2<String, String> stringStringTuple2) throws Exception {
+                        return stringStringTuple2._1();
+                    }
+                }
+        );
 
-//        ResultScanner rs = null;
-//        try {
-//            HTableInterface table = HbasePoolUtils.getHTable("gycrawler");
-//            rs = table.getScanner(scan);
-//            int i = 0;
-//            for (Result r : rs) {
-//                i++;
-//                for (Cell cell : r.rawCells()) {
-//                    String s = Bytes.toString(CellUtil.cloneRow(cell));
-//                    String s1 = Bytes.toString(CellUtil.cloneQualifier(cell));
-//                    String s2 = Bytes.toString(CellUtil.cloneValue(cell));
-//                    System.out.println("value: " + s2);
-//                }
-//                System.out.println("-------------------------------------------");
-//                for (KeyValue kv : r.list()) {
-//                    System.out.println("timestamp:" + kv.getTimestamp());
-//                    System.out.println("value: " +
-//                            Bytes.toString(kv.getValue()));
-//                }
-//                System.out.println("-----------------imu--------------------------");
-//                System.out.println(i);
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            rs.close();
-//        }
-
+        //空白栏目存入HDFS
+        emptyUrl.saveAsTextFile("/AnalysisModel/data_"+System.currentTimeMillis()+".txt");
 
     }
 
